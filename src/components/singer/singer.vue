9664e5cd3ce4ae3dfadeee1345b9f661a07fd47c
zht <877070017@qq.com>
2019-05-13 17:36:04 +0800

    三四节课程提交

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>歌手页面</div>
+</template>
+
+<script type="text/ecmascript">
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
+import 'common/stylus/base.styl'
+
+// import 'common/stylus/index.styl'
 Vue.config.productionTip = false
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
